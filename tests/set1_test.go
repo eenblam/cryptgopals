351c1aef36360d7257cebd806b72709ee926162c
eenblam <ben.a.elam@gmail.com>
2017-08-23 15:17:31 -0700

    Add 1.1

---
+++ tests/set1_test.go
@@ -0,0 +1,22 @@
+package set1
+
+import (
+    "fmt"
+    "testing"
+
+    "github.com/eenblam/cryptgopals/encode"
+)
+
+func TestHexToBase64(t *testing.T) {
+    inputHex := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    expectedBase64 := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    output, err := encode.HexToBase64(inputHex)
+    if err != nil {
+        fmt.Println(err)
+        t.Fail()
+    }
+    if output != expectedBase64 {
+        fmt.Println("Output did not match expected base64")
+        t.Fail()
+    }
+}
